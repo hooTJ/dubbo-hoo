37d02ac86f7a5248afd625b776c20dfca5870a42
hooTJ <953475015@qq.com>
2019-07-04 10:29:04 +0800

    注释

--- src/main/java/framework/ProtocolFactory.java
+++ src/main/java/framework/ProtocolFactory.java
@@ -3,23 +3,30 @@ package framework;
 import protocol.dubbo.DubboProtocol;
 import protocol.http.HttpProtocol;
 
+import java.util.ServiceLoader;
+
 /**
  * 协议工厂类
  */
 public class ProtocolFactory {
 
     public static Protocol getProtocol() {
-        Protocol protocol;
-        switch (System.getProperty("protocolName","dubbo")) {
-            case "http":
-                protocol = new HttpProtocol();
-                break;
-            case "dubbo":
-                protocol = new DubboProtocol();
-                break;
-            default:
-                protocol = new DubboProtocol();
-                break;
+        // SPI
+        ServiceLoader<Protocol> serviceLoader = ServiceLoader.load(Protocol.class);
+        Protocol protocol = serviceLoader.iterator().next();
+        if (protocol==null){
+            // 系统属性
+            switch (System.getProperty("protocolName","dubbo")) {
+                case "http":
+                    protocol = new HttpProtocol();
+                    break;
+                case "dubbo":
+                    protocol = new DubboProtocol();
+                    break;
+                default:
+                    protocol = new DubboProtocol();
+                    break;
+            }
         }
         return protocol;
     }
